4c0530316037183b32a022916998724c0a951776
Sissy Wang <sissy.wang@outlook.com>
2016-09-04 14:40:18 -0500

    Pseudo Code

---
+++ Lesson_2/Pseudo_Code.rb
@@ -0,0 +1,59 @@
+#1 write a method that returns the sum of two integers
+ #casual 
+ given two integers, number1 and number2
+ add number 1 and number 2 
+ puts the result 
+
+ #formal
+ START
+
+ #Given two integers 
+ PUTS number1+number2 
+
+ END
+
+ #2 write a method that takes an array of strings, and returns a string that is all those strings concatenated together
+ #casual 
+ given an array of string
+ def a method that :
+    - concatenate those strings 
+
+after all those, print the result
+
+#formal
+START
+
+#Given an array of strings 
+DEF a method 
+  - concatenate those strings 
+
+PRINT the result of the concatenateion 
+
+END 
+
+
+#3 write a method that takes an array of integers, and returns a new array with every other element
+#casual
+given an array of integers 
+def a method that:
+  - iterate through the array 
+  - print out the element of the array only when the index equals to 0,2,4,6,8,...,even number 
+  -put the numbers selected into a new array 
+
+after those, print out the new array 
+
+#formal
+START
+
+SET iterator = 1
+SET new array = values when iterator at odd numbers 
+
+
+DEF a method
+  - iterate through the array 
+  - print out the element of the array only when the iterator equals to odd numbers
+  -put the numbers selected into a new array 
+
+PRINT new array 
+
+END 
