574e541b3dea4ee61b8b9c181cbcad1ae53429ed
Sissy Wang <sissy.wang@outlook.com>
2016-09-06 16:57:43 -0500

    Car loan calculator

---
+++ Lesson_2/Car_Loan_Calculator.rb
@@ -0,0 +1,70 @@
+# monthly interest rate = APR/12 
+# Psudo 
+#  - ask for the 3 peices of info 
+#  - make sure those info are valid numbers 
+#  - calculate monthly rate: APR/12 
+#  - calculate loan duration: year*12
+#  - print out the result 
+
+def integer?(input)
+  input.to_i.to_s == input
+end 
+
+def float?(input)
+  input.to_f.to_s == input
+end 
+
+def valid_number?(input)
+  integer?(input) || float?(input)
+end
+
+
+
+puts ("Welcome to car loan calculator, let's calculate! ")
+
+loop do
+
+loan_amount = ''
+loop do
+puts ("Tell me the loan amount you have: ")
+loan_amount = gets.chomp
+if valid_number?(loan_amount)
+  break
+else 
+  puts "Hm, this is not a valid number"
+end 
+end 
+
+apr=''
+loop do
+puts ("Tell me the APR: ")
+apr = gets.chomp
+if valid_number?(apr)
+  break
+else 
+  puts "Hm, this is not a valid number"
+end 
+end 
+
+duration = ''
+loop do
+puts ("Tell me the loan duration in years:")
+duration = gets.chomp 
+if valid_number?(duration)
+  break
+else 
+  puts "Hm, this is not a valid number"
+end 
+end 
+
+result1 = apr.to_f / 12 
+result2 = duration.to_i*12
+puts("So, your monthly interest rate is #{result1}, and the loan duration in month is #{result2}.")
+
+puts("Do you want to calculate again?(Y for yes)")
+answer = gets.chomp
+break unless answer.downcase().start_with?('y')
+
+puts "Thanks for using this car loan calculator"
+
+end 
