d790d0a8d045840b44dd1e1b4a0ac6b38bc126be
Sissy Wang <sissy.wang@outlook.com>
2016-09-18 18:13:20 -0500

    medium 3

---
+++ Lesson_3/medium_3.rb
@@ -0,0 +1,187 @@
+#1
+def fun_with_ids
+  a_outer = 42
+  b_outer = "forty two"
+  c_outer = [42]
+  d_outer = c_outer[0] #0 means index 0, which is 42
+
+  a_outer_id = a_outer.object_id #=> an integers i object id = i*2+1. in this case, 42*2+1=85
+  b_outer_id = b_outer.object_id
+  c_outer_id = c_outer.object_id
+  d_outer_id = d_outer.object_id
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} before the block." #42;85
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} before the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} before the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} before the block.\n\n"
+
+  1.times do #this is a method under the first method
+    a_outer_inner_id = a_outer.object_id
+    b_outer_inner_id = b_outer.object_id
+    c_outer_inner_id = c_outer.object_id
+    d_outer_inner_id = d_outer.object_id
+
+    puts "a_outer id was #{a_outer_id} before the block and is: #{a_outer_inner_id} inside the block." #85;85
+    puts "b_outer id was #{b_outer_id} before the block and is: #{b_outer_inner_id} inside the block."
+    puts "c_outer id was #{c_outer_id} before the block and is: #{c_outer_inner_id} inside the block."
+    puts "d_outer id was #{d_outer_id} before the block and is: #{d_outer_inner_id} inside the block.\n\n"
+
+    a_outer = 22 #value got reassigned
+    b_outer = "thirty three"
+    c_outer = [44]
+    d_outer = c_outer[0]
+
+    puts "a_outer inside after reassignment is #{a_outer} with an id of: #{a_outer_id} before and: #{a_outer.object_id} after." #22; 85; 45
+    puts "b_outer inside after reassignment is #{b_outer} with an id of: #{b_outer_id} before and: #{b_outer.object_id} after."
+    puts "c_outer inside after reassignment is #{c_outer} with an id of: #{c_outer_id} before and: #{c_outer.object_id} after."
+    puts "d_outer inside after reassignment is #{d_outer} with an id of: #{d_outer_id} before and: #{d_outer.object_id} after.\n\n"
+
+
+    a_inner = a_outer
+    b_inner = b_outer
+    c_inner = c_outer
+    d_inner = c_inner[0]
+
+    a_inner_id = a_inner.object_id
+    b_inner_id = b_inner.object_id
+    c_inner_id = c_inner.object_id
+    d_inner_id = d_inner.object_id
+
+    puts "a_inner is #{a_inner} with an id of: #{a_inner_id} inside the block (compared to #{a_outer.object_id} for outer)." #22; 45; 45
+    puts "b_inner is #{b_inner} with an id of: #{b_inner_id} inside the block (compared to #{b_outer.object_id} for outer)."
+    puts "c_inner is #{c_inner} with an id of: #{c_inner_id} inside the block (compared to #{c_outer.object_id} for outer)."
+    puts "d_inner is #{d_inner} with an id of: #{d_inner_id} inside the block (compared to #{d_outer.object_id} for outer).\n\n"
+  end
+
+#once we leave the block, those variables that were defined inside the block lose their meaning.
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} BEFORE and: #{a_outer.object_id} AFTER the block." #since we defined a_outer once outside of the time method, so the one in the second method make sense. 22; 85, since a_outer_id only been talked about outside the method; 45
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} BEFORE and: #{b_outer.object_id} AFTER the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} BEFORE and: #{c_outer.object_id} AFTER the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} BEFORE and: #{d_outer.object_id} AFTER the block.\n\n"
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} INSIDE and: #{a_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh" #a inner only been defined inside the time method, so if we call it here, it will cause an error
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} INSIDE and: #{b_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} INSIDE and: #{c_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} INSIDE and: #{d_inner.object_id} AFTER the block.\n\n" rescue puts "ugh ohhhhh"
+end
+
+
+
+#2
+def fun_with_ids
+  a_outer = 42
+  b_outer = "forty two"
+  c_outer = [42]
+  d_outer = c_outer[0]
+
+  a_outer_id = a_outer.object_id
+  b_outer_id = b_outer.object_id
+  c_outer_id = c_outer.object_id
+  d_outer_id = d_outer.object_id
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} before the block."  #everything the same
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} before the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} before the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} before the block.\n\n"
+
+#Here comes another method, so this method gonna print out beofre the following two
+  an_illustrative_method(a_outer, b_outer, c_outer, d_outer, a_outer_id, b_outer_id, c_outer_id, d_outer_id)
+
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} BEFORE and: #{a_outer.object_id} AFTER the method call."
+  #this method has no access to the second method
+  #42; 85; 85
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} BEFORE and: #{b_outer.object_id} AFTER the method call."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} BEFORE and: #{c_outer.object_id} AFTER the method call."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} BEFORE and: #{d_outer.object_id} AFTER the method call.\n\n"
+
+#since a_inner is nodefined in this method, this gonna cause an error
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} INSIDE and: #{a_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} INSIDE and: #{b_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} INSIDE and: #{c_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} INSIDE and: #{d_inner.object_id} AFTER the method.\n\n" rescue puts "ugh ohhhhh"
+end
+
+
+
+
+def an_illustrative_method(a_outer, b_outer, c_outer, d_outer, a_outer_id, b_outer_id, c_outer_id, d_outer_id)
+  a_outer_inner_id = a_outer.object_id
+  b_outer_inner_id = b_outer.object_id
+  c_outer_inner_id = c_outer.object_id
+  d_outer_inner_id = d_outer.object_id
+
+  puts "a_outer id was #{a_outer_id} before the method and is: #{a_outer.object_id} inside the method." #85, since a_outter_id only defined in the first method;
+  # 85, since the new a_outter in this method is not defined yet
+  puts "b_outer id was #{b_outer_id} before the method and is: #{b_outer.object_id} inside the method."
+  puts "c_outer id was #{c_outer_id} before the method and is: #{c_outer.object_id} inside the method."
+  puts "d_outer id was #{d_outer_id} before the method and is: #{d_outer.object_id} inside the method.\n\n"
+
+  a_outer = 22  # new a_outter defined, this is a new variable, not the same one as in the first method
+  b_outer = "thirty three"
+  c_outer = [44]
+  d_outer = c_outer[0]
+
+  puts "a_outer inside after reassignment is #{a_outer} with an id of: #{a_outer_id} before and: #{a_outer.object_id} after."
+  #22; 85; 45, since this will be the id for the new defined a_outter
+  puts "b_outer inside after reassignment is #{b_outer} with an id of: #{b_outer_id} before and: #{b_outer.object_id} after."
+  puts "c_outer inside after reassignment is #{c_outer} with an id of: #{c_outer_id} before and: #{c_outer.object_id} after."
+  puts "d_outer inside after reassignment is #{d_outer} with an id of: #{d_outer_id} before and: #{d_outer.object_id} after.\n\n"
+
+
+  a_inner = a_outer
+  b_inner = b_outer
+  c_inner = c_outer
+  d_inner = c_inner[0]
+
+  a_inner_id = a_inner.object_id
+  b_inner_id = b_inner.object_id
+  c_inner_id = c_inner.object_id
+  d_inner_id = d_inner.object_id
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} inside the method (compared to #{a_outer.object_id} for outer)."
+  #22; 45; 45    all those numbers are for the new variables defined in this method
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} inside the method (compared to #{b_outer.object_id} for outer)."
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} inside the method (compared to #{c_outer.object_id} for outer)."
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} inside the method (compared to #{d_outer.object_id} for outer).\n\n"
+end
+
+#3
+My string looks like this now: pumpkins
+My array looks like this now: ["pumpkins", "rutabaga"]
+
+The reason is: 
+ruby pass value as pass a refenrence into objects. While passing values using a method,
+This method will create a new variable, and this variable will only be valable inside the method scope
+when we use String operator +=, this operation is reassign and created a new variable inside the scope of the method
+When we call it after the method finished, this variable is already forgotten
+<< operator will change the original object, so when we call it, it will call the changed original object
+
+#4
+My string looks like this now: pumpkinrutabaga
+My array looks like this now: ["pumpkins"]
+
+The reason is:
+<< modified the original variable my_string, it add rutabaga after the original string refenrence: pumpkins
+= can not modify the original array, so as soon as the new array show up, the old array inside the method is forgotten. 
+
+#5
+def color_valid(color)
+  color == "blue" || color == "green"
+end 
+
+reason: 
+ruby will automatically evaluates statement
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Lesson_3/test.rb
+++ Lesson_3/test.rb
@@ -1,9 +1,11 @@
-def foo(param = "no")
-  "yes"
+def tricky_method(a_string_param, an_array_param)
+  a_string_param << "rutabaga"
+  an_array_param << "rutabaga"
 end
 
-def bar(param = "no")
-  param == "no" ? "yes" : "no"
-end
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+tricky_method(my_string, my_array)
 
-p bar(foo)
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
