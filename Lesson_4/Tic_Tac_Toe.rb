7ce501b9322bd132fd83147160778deb2a094ec1
Sissy Wang <sissy.wang@outlook.com>
2016-09-23 12:07:01 -0500

    TTT change

--- Lesson_4/Tic_Tac_Toe.rb
+++ Lesson_4/Tic_Tac_Toe.rb
@@ -68,9 +68,9 @@ def detect_winner(brd)
        brd[line[2]] == PLAYER_MARKER
        return 'Player'
      elsif 
-       brd[line[0]] == PLAYER_MARKER && 
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
+       brd[line[0]] == COMPUTER_MARKER && 
+       brd[line[1]] == COMPUTER_MARKER &&
+       brd[line[2]] == COMPUTER_MARKER
       return 'Computer'
     end 
   end
