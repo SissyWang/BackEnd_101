5d4b577f4264cf7df36282f95e034fcae4bd96ca
Sissy Wang <sissy.wang@outlook.com>
2016-09-24 17:12:10 -0500

    rubocop_ttt

--- Lesson_4/TTT_Bonus.rb
+++ Lesson_4/TTT_Bonus.rb
@@ -1,7 +1,3 @@
-WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
-                [[2, 5, 8], [1, 4, 7], [3, 6, 9]] +
-                [[1, 5, 9], [3, 5, 7]]
-
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -10,7 +6,6 @@ def prompt(msg)
   puts "=> #{msg} "
 end
 
-# rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts "You are a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
@@ -27,34 +22,38 @@ def display_board(brd)
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
   puts "     |     |"
   puts " "
-end
-# rubocop:enable Metrics/AbcSize
+end 
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
   new_board
+end 
+
+def joinor(arr, delimiter = ', ', word = 'or')
+  arr[-1] = "#{word} #{arr.last}" if arr.size > 1
+  arr.size == 2? arr.join(' ') : arr.join(delimiter)
 end
 
 def empty_square(brd)
-  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
-end
+  brd.keys.select{|num| brd[num] == INITIAL_MARKER} 
+end 
 
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square ( #{empty_square(brd).join(', ')} ):"
+    prompt "Choose a square: #{joinor(empty_square(brd),', ' )}:"
     square = gets.chomp.to_i
     break if empty_square(brd).include?(square)
     prompt "Soorry, that's not a valid choice"
   end
-  brd[square] = PLAYER_MARKER
-end
+  brd[square] = PLAYER_MARKER 
+end 
 
 def computer_places_piece!(brd)
   square = empty_square(brd).sample
   brd[square] = COMPUTER_MARKER
-end
+end 
 
 def board_full?(brd)
   empty_square(brd).empty?
@@ -62,17 +61,25 @@ end
 
 def someone_won?(brd)
   !!detect_winner(brd)
-end
+end 
 
 def detect_winner(brd)
-  WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(PLAYER_MARKER) == 3
-      return "player"
-    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
-      return 'computer'
-    end
+  winning_lines = [[1,2,3],[4,5,6],[7,8,9]] +
+                  [[2,5,8],[1,4,7],[3,6,9]] +
+                  [[1,5,9],[3,5,7]]
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER && 
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+       return 'Player'
+     elsif 
+       brd[line[0]] == COMPUTER_MARKER && 
+       brd[line[1]] == COMPUTER_MARKER &&
+       brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end 
   end
-  nil
+  nil              
 end
 
 loop do
@@ -92,13 +99,13 @@ loop do
 
   if someone_won?(board)
     prompt"#{detect_winner(board)} won!"
-  else
+  else 
     prompt"It's a tie!"
   end
 
   prompt"Play again?(y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
-end
+end 
 
 prompt "Thanks for playing Tic Tac Toe. Good bye!"
--- Lesson_4/test.rb
+++ Lesson_4/test.rb
@@ -1,141 +1,15 @@
-# 1. display board : draw a board use lines, and thendisplay it
-# 2. find a way to define each empty square in the board 
-#   1). define initial marker, player marker and computer marker 
-# 3. pass the initial marker into the black spaces 
-# 4. ask the user to choose a square 
-#    1). define a ask for input method
-#    2). make sure the input is valid, if not, loop back
-#    3). valid means: 1-9, and everytime you choose a number(stand for a spacein the board), this number become invalid
-#          1. define a new method : empty_square
-#             select all the keys from board key-value pair, and set them all = initial_marker
-#           2. if the number inserted is included in the array selected, then mark this number as X
-#              if not, break loop
-# 5.  display the updated board 
-# 6. computer choose a square: choose a random square from all squares availiable (the elements left in empty_square array)
-#     and then mark it as computer marker
-# 7. display the updated board
-# 8. decide if the board is full: board full = empty_square is empty 
-# 9. decided who won
-#     1) figure out the winning condition 
-#     2) iterate through and figure out the winner 
-#     3) won only when the board not full
-#     4) is player win and board not full? is computer win and board not full? if tie?
-#     5) prompt the winner 
-# 10. play again? 
+# joinor([1, 2, 3])                # => "1, 2, or 3"
+# joinor([1, 2, 3], '; ')          # => "1; 2; or 3"
+# joinor([1, 2, 3], ', ', 'and')   # => "1, 2, and 3"
 
-# * 3 loops in total 
-#  1) easist loop : play again loop 
-#  2) main game loop : if there is still element left in empty_square, go back to empty_square until nothing left
-#  3) display loop : if noone wins, keep looping and refresh the board 
-require 'pry'
 
-INITIAL_MARKER = ' '
-PLAYER_MARKER = 'X'
-COMPUTER_MARKER = 'O'
-
-def prompt(msg)
-  puts "=>#{msg}"
-end 
-
-def display_board(brd)
-  system 'clear'
-  puts " "
-  puts "     |     |     "
-  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}  "
-  puts "     |     |     "
-  puts "-----+-----+-----"
-  puts "     |     |     "
-  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}  "
-  puts "     |     |     "
-  puts "-----+-----+-----"
-  puts "     |     |     "
-  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}  "
-  puts "     |     |     "
-  puts " "
-end 
-
-def initialize_board
-  new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
-  new_board
+def joinor(arr, delimiter=', ', word='or')
+  arr[-1] = "#{word} #{arr.last}" if arr.size > 1 # arr[-1] permanently mutating the last element in the arr
+  arr.size == 2 ? arr.join(' ') : arr.join(delimiter) # arr.size still == 3, because there is 1,2,3. and arr[-1] mutating the last element, but not deleting it. so there is still 3 elements. 
+                                                      # if arr.size ==2, just join them together. if not, put , in between
 end
 
-def empty_square(brd)
-  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
-end 
-
-def play_places_piece!(brd)
-square = ''
-  loop do
-    prompt "Choose your square(#{empty_square(brd).join(',')}):"
-    square = gets.chomp.to_i
-    break if empty_square(brd).include?(square)
-    prompt "Not a valid answer"
-  end 
-  brd[square] = PLAYER_MARKER
-end 
-
-def computer_places_piece!(brd)
-  square = empty_square(brd).sample
-  brd[square] = COMPUTER_MARKER
-end 
-
-def board_full?(brd)
-  empty_square(brd).empty?
-end 
-
-def someone_won?(brd)
-  !!detect_winner(brd)
-end 
-
-def detect_winner(brd)
-  winning_lines = [[1,2,3],[4,5,6],[7,8,9]] + 
-                  [[1,4,7],[2,5,8],[3,6,9]] +
-                  [[1,5,9],[3,5,7]]
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
-       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-         brd[line[1]] == COMPUTER_MARKER &&
-         brd[line[2]] == COMPUTER_MARKER
-         return 'Computer'
-    end
-  end
-  nil
-end
-
-loop do
-board = initialize_board
-
-  loop do
-    display_board(board)
-
-    play_places_piece!(board)
-    binding.pry
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
-    binding.pry
-    break if someone_won?(board) || board_full?(board)
-  end
-
-  display_board(board)
-
-  if someone_won?(board)
-    prompt"#{detect_winner(board)} won"
-  else 
-    prompt"It's a tie"
-  end 
-
-    prompt"Play again?"
-    answer = gets.chomp
-    break unless answer.downcase.start_with?('y')
-end 
-
-prompt "Thank you"
-
+puts joinor([1,2,3,4,5,6,7,8],';','and')
 
 
 
